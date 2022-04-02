8a8a64bcb5a9023039bc50772b347a8bf0472908
Elizabeth Atkinson <eatkinso@illinois.edu>
2022-04-02 17:22:40 -0500

    daily notes EA

---
+++ Firmware/ST/STM32Cube_FW_WL_V1.1.0/Projects/NUCLEO-WL55JC/Applications/LoRaWAN/LoRaWAN_AT_Slave_DualCore/CM4/LoRaWAN/App/lora_app.c
@@ -0,0 +1,322 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    lora_app.c
+  * @author  MCD Application Team
+  * @brief   Application of the LRWAN Middleware
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "platform.h"
+#include "Region.h" /* Needed for LORAWAN_DEFAULT_DATA_RATE */
+#include "sys_app.h"
+#include "lora_app.h"
+#include "stm32_seq.h"
+#include "stm32_timer.h"
+#include "utilities_def.h"
+#include "lora_app_version.h"
+#include "LmHandler.h"
+#include "lora_command.h"
+#include "lora_at.h"
+#include "mbmuxif_sys.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* External variables ---------------------------------------------------------*/
+/* USER CODE BEGIN EV */
+
+/* USER CODE END EV */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private function prototypes -----------------------------------------------*/
+/**
+  * @brief  join event callback function
+  * @param  joinParams status of join
+  */
+static void OnJoinRequest(LmHandlerJoinParams_t *joinParams);
+
+/**
+  * @brief  tx event callback function
+  * @param  params status of last Tx
+  */
+static void OnTxData(LmHandlerTxParams_t *params);
+
+/**
+  * @brief callback when LoRa application has received a frame
+  * @param appData data received in the last Rx
+  * @param params status of last Rx
+  */
+static void OnRxData(LmHandlerAppData_t *appData, LmHandlerRxParams_t *params);
+
+/*!
+ * Will be called each time a Radio IRQ is handled by the MAC layer
+ *
+ * \remark OnMacProcessNotify not needed on dual Core as OnMacProcessNotify is processed on M0+
+ */
+static void OnMacProcessNotify(void);
+
+/**
+  * @brief  call back when LoRaWan Stack needs update
+  */
+static void CmdProcessNotify(void);
+
+/* USER CODE BEGIN PFP */
+
+/**
+  * @brief  LED Tx timer callback function
+  * @param  context ptr of LED context
+  */
+static void OnTxTimerLedEvent(void *context);
+
+/**
+  * @brief  LED Rx timer callback function
+  * @param  context ptr of LED context
+  */
+static void OnRxTimerLedEvent(void *context);
+
+/**
+  * @brief  LED Join timer callback function
+  * @param  context ptr of LED context
+  */
+static void OnJoinTimerLedEvent(void *context);
+
+/* USER CODE END PFP */
+
+/* Private variables ---------------------------------------------------------*/
+/**
+  * @brief LoRaWAN handler Callbacks
+  */
+static LmHandlerCallbacks_t LmHandlerCallbacks =
+{
+  .GetBatteryLevel =           GetBatteryLevel,
+  .GetTemperature =            GetTemperatureLevel,
+  .OnMacProcess =              OnMacProcessNotify,
+  .OnJoinRequest =             OnJoinRequest,
+  .OnTxData =                  OnTxData,
+  .OnRxData =                  OnRxData
+};
+
+/**
+  * @brief LoRaWAN handler parameters
+  */
+static LmHandlerParams_t LmHandlerParams =
+{
+  .ActiveRegion =             ACTIVE_REGION,
+  .DefaultClass =             LORAWAN_DEFAULT_CLASS,
+  .AdrEnable =                LORAWAN_ADR_STATE,
+  .TxDatarate =               LORAWAN_DEFAULT_DATA_RATE,
+  .PingPeriodicity =          LORAWAN_DEFAULT_PING_SLOT_PERIODICITY
+};
+
+/* USER CODE BEGIN PV */
+/**
+  * @brief Timer to handle the application Tx Led to toggle
+  */
+static UTIL_TIMER_Object_t TxLedTimer;
+
+/**
+  * @brief Timer to handle the application Rx Led to toggle
+  */
+static UTIL_TIMER_Object_t RxLedTimer;
+
+/**
+  * @brief Timer to handle the application Join Led to toggle
+  */
+static UTIL_TIMER_Object_t JoinLedTimer;
+
+/* USER CODE END PV */
+
+/* Exported functions ---------------------------------------------------------*/
+/* USER CODE BEGIN EF */
+
+/* USER CODE END EF */
+
+void LoRaWAN_Init(void)
+{
+  FEAT_INFO_Param_t *p_cm0plus_specific_features_info;
+  uint32_t lora_cm0plus_app;
+
+  CMD_Init(CmdProcessNotify);
+
+  /* USER CODE BEGIN LoRaWAN_Init_1 */
+
+  BSP_LED_Init(LED_BLUE);
+  BSP_LED_Init(LED_GREEN);
+  BSP_LED_Init(LED_RED);
+  BSP_PB_Init(BUTTON_SW2, BUTTON_MODE_EXTI);
+
+  /* Get CM4 LoRa APP version*/
+  APP_LOG(TS_OFF, VLEVEL_M, "M4 APP_VERSION:     V%X.%X.%X\r\n",
+          (uint8_t)(__CM4_APP_VERSION >> __APP_VERSION_MAIN_SHIFT),
+          (uint8_t)(__CM4_APP_VERSION >> __APP_VERSION_SUB1_SHIFT),
+          (uint8_t)(__CM4_APP_VERSION >> __APP_VERSION_SUB2_SHIFT));
+
+  /* Get CM0 LoRa APP version*/
+  p_cm0plus_specific_features_info = MBMUXIF_SystemGetFeatCapabInfoPtr(FEAT_INFO_SYSTEM_ID);
+  lora_cm0plus_app = p_cm0plus_specific_features_info->Feat_Info_Feature_Version ;
+  APP_LOG(TS_OFF, VLEVEL_M, "M0PLUS_APP_VERSION: V%X.%X.%X\r\n",
+          (uint8_t)(lora_cm0plus_app >> __APP_VERSION_MAIN_SHIFT),
+          (uint8_t)(lora_cm0plus_app >> __APP_VERSION_SUB1_SHIFT),
+          (uint8_t)(lora_cm0plus_app >> __APP_VERSION_SUB2_SHIFT));
+
+  /* Get MW LoraWAN info */
+  p_cm0plus_specific_features_info = MBMUXIF_SystemGetFeatCapabInfoPtr(FEAT_INFO_LORAWAN_ID);
+  lora_cm0plus_app = p_cm0plus_specific_features_info->Feat_Info_Feature_Version ;
+  APP_LOG(TS_OFF, VLEVEL_M, "MW_LORAWAN_VERSION: V%X.%X.%X\r\n",
+          (uint8_t)(lora_cm0plus_app >> __APP_VERSION_MAIN_SHIFT),
+          (uint8_t)(lora_cm0plus_app >> __APP_VERSION_SUB1_SHIFT),
+          (uint8_t)(lora_cm0plus_app >> __APP_VERSION_SUB2_SHIFT));
+
+  /* Get MW SubGhz_Phy info */
+  p_cm0plus_specific_features_info = MBMUXIF_SystemGetFeatCapabInfoPtr(FEAT_INFO_RADIO_ID);
+  lora_cm0plus_app = p_cm0plus_specific_features_info->Feat_Info_Feature_Version ;
+  APP_LOG(TS_OFF, VLEVEL_M, "MW_RADIO_VERSION:   V%X.%X.%X\r\n",
+          (uint8_t)(lora_cm0plus_app >> __APP_VERSION_MAIN_SHIFT),
+          (uint8_t)(lora_cm0plus_app >> __APP_VERSION_SUB1_SHIFT),
+          (uint8_t)(lora_cm0plus_app >> __APP_VERSION_SUB2_SHIFT));
+  UTIL_TIMER_Create(&TxLedTimer, 0xFFFFFFFFU, UTIL_TIMER_ONESHOT, OnTxTimerLedEvent, NULL);
+  UTIL_TIMER_Create(&RxLedTimer, 0xFFFFFFFFU, UTIL_TIMER_ONESHOT, OnRxTimerLedEvent, NULL);
+  UTIL_TIMER_Create(&JoinLedTimer, 0xFFFFFFFFU, UTIL_TIMER_PERIODIC, OnJoinTimerLedEvent, NULL);
+  UTIL_TIMER_SetPeriod(&TxLedTimer, 500);
+  UTIL_TIMER_SetPeriod(&RxLedTimer, 500);
+  UTIL_TIMER_SetPeriod(&JoinLedTimer, 500);
+
+  /* USER CODE END LoRaWAN_Init_1 */
+
+  UTIL_SEQ_RegTask((1 << CFG_SEQ_Task_MbLmHandlerProcess), UTIL_SEQ_RFU, LmHandlerProcess);
+  UTIL_SEQ_RegTask((1 << CFG_SEQ_Task_MbVcom), UTIL_SEQ_RFU, CMD_Process);
+  /* Init the Lora Stack*/
+  LmHandlerInit(&LmHandlerCallbacks);
+
+  LmHandlerConfigure(&LmHandlerParams);
+
+  /* USER CODE BEGIN LoRaWAN_Init_Last */
+  UTIL_TIMER_Start(&JoinLedTimer);
+
+  APP_PPRINTF("ATtention command interface\r\n");
+  APP_PPRINTF("AT? to list all available functions\r\n");
+
+  /* USER CODE END LoRaWAN_Init_Last */
+}
+
+/* USER CODE BEGIN PB_Callbacks */
+
+/* USER CODE END PB_Callbacks */
+
+/* Private functions ---------------------------------------------------------*/
+/* USER CODE BEGIN PrFD */
+
+/* USER CODE END PrFD */
+
+static void OnRxData(LmHandlerAppData_t *appData, LmHandlerRxParams_t *params)
+{
+  /* USER CODE BEGIN OnRxData_1 */
+  if ((appData != NULL) || (params != NULL))
+  {
+    BSP_LED_On(LED_BLUE) ;
+
+    UTIL_TIMER_Start(&RxLedTimer);
+
+    AT_event_receive(appData, params);
+  }
+  /* USER CODE END OnRxData_1 */
+}
+
+/* USER CODE BEGIN PrFD_LedEvents */
+static void OnTxTimerLedEvent(void *context)
+{
+  BSP_LED_Off(LED_GREEN) ;
+}
+
+static void OnRxTimerLedEvent(void *context)
+{
+  BSP_LED_Off(LED_BLUE) ;
+}
+
+static void OnJoinTimerLedEvent(void *context)
+{
+  BSP_LED_Toggle(LED_RED) ;
+}
+
+/* USER CODE END PrFD_LedEvents */
+
+static void OnTxData(LmHandlerTxParams_t *params)
+{
+  /* USER CODE BEGIN OnTxData_1 */
+  if ((params != NULL))
+  {
+    /* Process Tx event only if its a mcps response to prevent some internal events (mlme) */
+    if (params->IsMcpsConfirm != 0)
+    {
+      BSP_LED_On(LED_GREEN) ;
+      UTIL_TIMER_Start(&TxLedTimer);
+    }
+    AT_event_confirm(params);
+  }
+  /* USER CODE END OnTxData_1 */
+}
+
+static void OnJoinRequest(LmHandlerJoinParams_t *joinParams)
+{
+  /* USER CODE BEGIN OnJoinRequest_1 */
+  if (joinParams != NULL)
+  {
+    if (joinParams->Status == LORAMAC_HANDLER_SUCCESS)
+    {
+      UTIL_TIMER_Stop(&JoinLedTimer);
+      BSP_LED_Off(LED_RED) ;
+
+    }
+    AT_event_join(joinParams);
+  }
+  /* USER CODE END OnJoinRequest_1 */
+}
+
+static void CmdProcessNotify(void)
+{
+  /* USER CODE BEGIN CmdProcessNotify_1 */
+
+  /* USER CODE END CmdProcessNotify_1 */
+  UTIL_SEQ_SetTask((1 << CFG_SEQ_Task_MbVcom), 0);
+  /* USER CODE BEGIN CmdProcessNotify_2 */
+
+  /* USER CODE END CmdProcessNotify_2 */
+}
+
+static void OnMacProcessNotify(void)
+{
+  /* USER CODE BEGIN OnMacProcessNotify_1 */
+
+  /* USER CODE END OnMacProcessNotify_1 */
+}
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- notebooks/notebook.md
+++ notebooks/notebook.md
@@ -97,13 +97,49 @@ Finish schematic design tasks by evening Saturday
 
 
 
+# 3/6/2022 Group PCB Work 
+
+- Each working on finishing PCB layout by Tues deadline
+- Alex finished the alarm board, nidhi finished RFID carrier board
+- Alex finished alarm BOM, Nidhi finished RFID BOM
+- Elizabeth working on main board
 
 
 
 
 
+# 4/2/2022 Hardware work 
 
 
+-- Elizabeth: working on running LoRa example application on STM32WL dev board. Pinout and instructions below. 
+
+![lora_devboard](lora_instructions_devboard.jpeg)
+
+![lora_devboard_pin](devboard_pinout.jpeg)
+
+Trying to run the LoRa demo on the board -- following directions below from UM2463 for STM32cubeide. First the blinky example (GPIO_EXTI), then one of the LoRa ones -- File path to LoRa project is `bike-anti-theft-device/Firmware/stm32wl_fwpackage/STM32Cube_FW_WL_V1.2.0/Projects/NUCLEO-WL55JC/Demonstrations/LocalNetwork/`. Running the "sensor" first -- setup is connected to Spectrum analyzer, want to verify that it can transmit something. 
 
 
+![first_ex](first_ex.png)
 
+
+NOTE: use IMPORT-> Import STM32CubeExample and then select the desired example. Otherwise it will not load builders and the project won't build (and I couldn't figure out how to add builders after the project has already been imported).
+
+Blinky worked! Now onto trying a LoRa example. Note that the LoRa examples require STM32CubeIDE 1.9+ apparently. 
+
+
+Back to the AT slave example...ugh. UART on linux: 
+
+screen /dev/ttyACM0 9600
+
+
+monitor serial: `cat -v /dev/ttyACM0`
+
+
+send command: `echo -e 'command' > /dev/ttyACM0`
+
+
+set serial to 9600 baud, 8 bit data, no parity, 1 stop bit: `stty -F /dev/ttyACM0 cs8 -cstopb -parenb`
+
+
+Successfully ran RF test at 400-500 MHz. Next time, populate board, work on porting LoRa example to our HW. 
